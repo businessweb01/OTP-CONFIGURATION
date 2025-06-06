bbd796b2a887e72e098783efa15418ca0b3a465b
FullStackDev. Eugene <103896624+businessweb01@users.noreply.github.com>
2025-06-06 15:14:20 +0800

    removed the send-otp

--- sendOTP.js
+++ sendOTP.js
@@ -10,7 +10,7 @@ function generateOtp() {
 }
 
 // Send OTP endpoint
-router.post('/send-otp', async (req, res) => {
+router.post('/', async (req, res) => {
   const { to_email } = req.body;
 
   if (!to_email) return res.status(400).json({ success: false, message: 'Email required' });
