30b38b16f16eeba3ac5e113aea88dca5a5f61289
FullStackDev. Eugene <103896624+businessweb01@users.noreply.github.com>
2025-08-16 10:04:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import sendOtpRoute from './sendOTP.js';
+import { default as sendOtpRoute } from './sendOTP.js'
 
 dotenv.config();
 const app = express();
